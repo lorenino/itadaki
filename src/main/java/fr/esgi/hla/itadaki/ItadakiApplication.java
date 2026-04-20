faa34269894319ad05822d4dab29a97ddfe051c5
Ahmed <letaiefahmedyahya@gmail.com>
2026-04-20 11:00:30 +0200

    Init du projet

---
+++ src/main/java/fr/esgi/hla/itadaki/ItadakiApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.hla.itadaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItadakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItadakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/hla/itadaki/ItadakiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.hla.itadaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItadakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
